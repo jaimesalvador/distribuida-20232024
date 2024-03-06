075dae3e1a852f3d5a0758d92b76edb20dee2abf
Jaime Salvador <jaime.salvador@ideasybits.com>
2024-03-06 11:15:08 -0500

    correccion-examen

--- app-authors-helidon/src/main/java/com/distribuida/authors/RestApp.java
+++
@@ -1,8 +0,0 @@
-package com.distribuida.authors;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApp extends Application {
-}
